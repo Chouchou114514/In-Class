40e1a16df1ebbad14ebfad57c1652bc1d61bb7f3
Junhao Zhong <jz455@duke.edu>
2024-01-31 12:05:05 -0500

    create py to database

---
+++ health_database.py
@@ -0,0 +1,14 @@
+"""
+Single patient record: [Name(str),age(int),MRN(int), Tests(list)]
+"""
+
+def create_database_entry(name, age, mrn):
+    new_patient = [name, age, mrn, []]
+    return new_patient
+
+def main():
+    x = create_database_entry("Ann", 36, 101)
+    print(x)
+
+if __name__ == "_main_":
+    main()
