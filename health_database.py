e4248cd99149c1eb38b424ac44697c623983a848
Junhao Zhong <jz455@duke.edu>
2024-01-31 12:38:27 -0500

    modify the py to add function

--- health_database.py
+++ health_database.py
@@ -1,14 +1,36 @@
 """
 Single patient record: [Name(str),age(int),MRN(int), Tests(list)]
+Test list: [("HDL",100)]
 """
 
 def create_database_entry(name, age, mrn):
     new_patient = [name, age, mrn, []]
     return new_patient
 
-def main():
-    x = create_database_entry("Ann", 36, 101)
-    print(x)
+def print_database(db):
+    for patient in db:
+        print("Name:{}, MRN:{}, Age:{}, List:{}".format(patient[0],patient[2],patient[1],patient[3]))
 
-if __name__ == "_main_":
+def get_patient(db, MRN):
+    for patient in db:
+        if patient[2] == MRN:
+            return patient
+
+def add_test_to_patient(db,MRN,test_name,test_value):
+    patient = get_patient(db,MRN)
+    patient[3].append([test_name,test_value])
+
+
+def main():
+    db = []
+    db.append(create_database_entry("Ann Ables", 36, 101))
+    db.append(create_database_entry("Bob Boylse",25,102))
+    db.append(create_database_entry("Chris Chou", 50, 103))
+
+    print_database(db)
+    add_test_to_patient(db, 101, "HDL", 100)
+    add_test_to_patient(db,101,"LDL", 102)
+    print()
+    print_database(db)
+if __name__ == "__main__":
     main()
